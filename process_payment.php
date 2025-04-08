aac4c99085844736ed46ea53527ae20418d0a2f6
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-09 00:30:48 +0200

    Create process_payment.php

---
+++ process_payment.php
@@ -0,0 +1,31 @@
+<?php
+// process_payment.php
+
+header('Content-Type: application/json');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$merchant_id = $data['merchant_id'];
+$merchant_key = $data['merchant_key'];
+$amount = $data['amount'];
+$return_url = $data['return_url'];
+$cancel_url = $data['cancel_url'];
+$notify_url = $data['notify_url'];
+
+$payfast_url = "https://www.payfast.co.za/eng/process";
+
+$paymentData = [
+    'merchant_id' => $merchant_id,
+    'merchant_key' => $merchant_key,
+    'amount' => number_format($amount, 2, '.', ''),
+    'item_name' => 'Full Chicken',
+    'return_url' => $return_url,
+    'cancel_url' => $cancel_url,
+    'notify_url' => $notify_url,
+];
+
+$query_string = http_build_query($paymentData);
+$redirect_url = "$payfast_url?$query_string";
+
+echo json_encode(['redirect_url' => $redirect_url]);
+?>
