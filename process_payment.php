4ee87794b43b35e7a0cd64229d3584b3488719b2
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-09 15:38:33 +0200

    Update process_payment.php

--- process_payment.php
+++ process_payment.php
@@ -1,18 +1,18 @@
 <?php
 // process_payment.php
-
 header('Content-Type: application/json');
 
 $data = json_decode(file_get_contents('php://input'), true);
 
 $merchant_id = getenv('MERCHANT_ID');
 $merchant_key = getenv('MERCHANT_KEY');
+
 $amount = $data['amount'];
 $return_url = $data['return_url'];
 $cancel_url = $data['cancel_url'];
 $notify_url = $data['notify_url'];
 
-$payfast_url = "https://www.payfast.co.za/eng/process";
+$payfast_url = "https://www.payfast.co.za/eng/process"; // Use sandbox URL for testing
 
 $paymentData = [
     'merchant_id' => $merchant_id,
@@ -21,11 +21,10 @@ $paymentData = [
     'item_name' => 'Full Chicken',
     'return_url' => $return_url,
     'cancel_url' => $cancel_url,
-    'notify_url' => $notify_url,
+    'notify_url' => $notify_url
 ];
 
 $query_string = http_build_query($paymentData);
 $redirect_url = "$payfast_url?$query_string";
 
 echo json_encode(['redirect_url' => $redirect_url]);
-?>
