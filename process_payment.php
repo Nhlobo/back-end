5588e4b454a28477cf07c0236cd366e814d95ceb
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-09 16:40:53 +0200

    Update process_payment.php

--- process_payment.php
+++ process_payment.php
@@ -13,14 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-$merchant_id = "15465428";
-$merchant_key = "0cyrfu755y7bg";
+// Use test PayFast credentials
+$merchant_id = "10000100";  // Sandbox Merchant ID
+$merchant_key = "46f0cd694581a";  // Sandbox Merchant Key
+
 $amount = $data['amount'];
 $return_url = $data['return_url'];
 $cancel_url = $data['cancel_url'];
 $notify_url = $data['notify_url'];
 
-$payfast_url = "https://www.payfast.co.za/eng/process";
+$payfast_url = "https://sandbox.payfast.co.za/eng/process";
 
 $paymentData = [
     'merchant_id' => $merchant_id,
