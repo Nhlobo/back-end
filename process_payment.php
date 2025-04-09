4f8416673cdb76ae0485bee686490d0a3f872148
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-09 18:27:10 +0200

    Update process_payment.php

--- process_payment.php
+++ process_payment.php
@@ -10,32 +10,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
+// Get payment data from frontend
 $data = json_decode(file_get_contents('php://input'), true);
 
-// Use test PayFast credentials
-$merchant_id = "10000100";  // Sandbox Merchant ID
-$merchant_key = "46f0cd694581a";  // Sandbox Merchant Key
-
+// Example PayFast credentials (replace with actual)
+$merchant_id = "10000100";
+$merchant_key = "46f0cd694581a";
 $amount = $data['amount'];
-$return_url = $data['return_url'];
-$cancel_url = $data['cancel_url'];
-$notify_url = $data['notify_url'];
+$item_name = "Product Name"; // Example product name
+$shipping_method = $data['shippingMethod']; // Either "delivery" or "collection"
+$return_url = "https://nhlobo.github.io/firt-try/return.html";
+$cancel_url = "https://nhlobo.github.io/firt-try/cancel.html";
+$notify_url = "https://back-end-2-kyrl.onrender.com/notify.php";
 
+// PayFast payment URL
 $payfast_url = "https://sandbox.payfast.co.za/eng/process";
 
+// Payment data to send to PayFast
 $paymentData = [
     'merchant_id' => $merchant_id,
     'merchant_key' => $merchant_key,
     'amount' => number_format($amount, 2, '.', ''),
-    'item_name' => 'Full Chicken',
+    'item_name' => $item_name,
     'return_url' => $return_url,
     'cancel_url' => $cancel_url,
-    'notify_url' => $notify_url,
+    'notify_url' => $notify_url
 ];
 
+// Build the query string
 $query_string = http_build_query($paymentData);
-$redirect_url = "$payfast_url?$query_string";
 
-echo json_encode(['redirect_url' => $redirect_url]);
+// Return PayFast URL for frontend to redirect to
+echo json_encode([
+    'payfast_redirect_url' => $payfast_url . '?' . $query_string
+]);
+exit();
 ?>
