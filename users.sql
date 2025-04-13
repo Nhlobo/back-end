f3ec5478489e20e80a02e3750197f2c40185aab1
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-13 13:03:06 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,11 +1,7 @@
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(100),
-  email VARCHAR(100),
-  password VARCHAR(255),
-  role ENUM('user', 'admin') DEFAULT 'user'
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullname VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Create one admin
-INSERT INTO users (username, email, password, role)
-VALUES ('Admin', 'admin@example.com', SHA2('admin123', 256), 'admin');
