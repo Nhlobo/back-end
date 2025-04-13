a1be55b9d8adfbafe1ce30cf7701fd52d16f74ce
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-13 13:55:31 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,4 +1,9 @@
 <?php
+// Allow frontend from GitHub Pages to access backend
+header("Access-Control-Allow-Origin: https://your-github-username.github.io");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
 include 'db.php';
 
 $stmt = $conn->prepare("SELECT * FROM products WHERE stock > 0 ORDER BY created_at DESC");
