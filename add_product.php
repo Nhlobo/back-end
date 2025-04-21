ba18ba77b8b93d5719d9622f493b59cb42c8f9fd
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-21 13:01:54 +0200

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -1,17 +1,22 @@
 <?php
 include 'db.php';
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$name = $data['name'];
-$desc = $data['description'];
-$price = $data['price'];
-$stock = $data['stock'];
-$image = $data['image_url'];
+$name = htmlspecialchars($data['name']);
+$desc = htmlspecialchars($data['description']);
+$price = floatval($data['price']);
+$stock = intval($data['stock']);
+$image = htmlspecialchars($data['image_url']);
 
-$sql = "INSERT INTO products (name, description, price, stock, image_url) 
+$sql = "INSERT INTO products (name, description, price, stock, image_url)
         VALUES ('$name', '$desc', '$price', '$stock', '$image')";
-mysqli_query($conn, $sql);
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(["message" => "Product added successfully"]);
+} else {
+    echo json_encode(["error" => "Error: " . $sql . "<br>" . $conn->error]);
+}
 ?>
