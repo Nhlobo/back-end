9c61e4d55e75c36c62e9c906caaeed338989c666
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2025-04-21 13:15:14 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,4 @@
 <?php
-// Allow frontend from GitHub Pages to access backend
-header("Access-Control-Allow-Origin: https://your-github-username.github.io");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-
 session_start();
 session_unset();
 session_destroy();
